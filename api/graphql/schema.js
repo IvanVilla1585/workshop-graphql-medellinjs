7573a13f1559da48fd73bc4b3f83c5d79e547f56
Ivan Villa <trezeguet55@gmail.com>
2018-01-28 16:06:04 -0500

    <graphql schema.js>
    configurate post schema and resolver

--- api/graphql/schema.js
+++ api/graphql/schema.js
@@ -5,15 +5,19 @@ const {makeExecutableSchema} = require('graphql-tools');
 
 const showSchema = require('./shows-tv/schema');
 const showResolrver = require('./shows-tv/resolver');
+const postSchema = require('./posts/schema');
+const postResolrver = require('./posts/resolver');
 
 
 const typeDefs = [
-  ...showSchema
+  ...showSchema,
+  ...postSchema
 ];
 
 
 const resolvers = merge(
-  showResolrver
+  showResolrver,
+  postResolrver
 );
 
 
