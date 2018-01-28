42a90154bec8fa2fd9c9c97d86d1b0587e1536f4
Ivan Villa <trezeguet55@gmail.com>
2018-01-28 16:06:33 -0500

    <api server.js>
    create mongodb connection

--- api/server.js
+++ api/server.js
@@ -1,15 +1,22 @@
 'use strict'
 
 const express = require('express');
+const mongoose = require('mongoose');
+const Promise = require('bluebird');
 const bodyParse = require('body-parser');
 const {graphqlExpress, graphiqlExpress} = require('apollo-server-express');
 
-const schema = require('./graphql/schema');
 const config = require('./config');
+const schema = require('./graphql/schema');
 const ShowConnector = require('./graphql/shows-tv/connector');
+const PostStorage = require('./graphql/posts/storage');
 
 const app = express();
 const port = 3000;
+
+mongoose.Promise = Promise;
+
+const conn = mongoose.createConnection(config.db);
 
 app.use(bodyParse.json());
 
@@ -17,7 +24,8 @@ app.use('/graphql', graphqlExpress(req => {
   return {
     schema,
     context: {
-      showConnector: new ShowConnector(config.tvMazeUrl)
+      showConnector: new ShowConnector(config.tvMazeUrl),
+      postStorage: new PostStorage(conn)
     }
   }
 }));
